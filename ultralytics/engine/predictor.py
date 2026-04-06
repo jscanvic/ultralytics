038a37f4e54fd6a3d3834bb747c06ae96673e4d1
Jérémy Scanvic <jscanvicz@gmail.com>
2026-04-06 15:08:30 +0200

    disable model fusion

--- ultralytics/engine/predictor.py
+++ ultralytics/engine/predictor.py
@@ -402,7 +402,7 @@ class BasePredictor:
             dnn=self.args.dnn,
             data=self.args.data,
             fp16=self.args.half,
-            fuse=True,
+            fuse=False,
             verbose=verbose,
         )
 
